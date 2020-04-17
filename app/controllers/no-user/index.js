7e74c8a446e7b1d235b888678a541fbc80f10316
islizeqiang <islizeqiang@gmail.com>
2020-04-17 23:43:48 +0800

    feat: add request

--- app/controllers/no-user/index.js
+++
@@ -1,1 +0,0 @@
-exports.recommend = require('./recommend');
--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,10 @@
 const Router = require('koa-router');
 const miscController = require('./controllers/misc');
-const search = require('./controllers/search');
 
 const router = new Router();
 
 router.get('/', miscController.getApiInfo);
 router.get('/spec', miscController.getSwaggerSpec);
 router.get('/status', miscController.healthcheck);
-
-// 获取不需要登录的相关接口
-router.get('/search', search);
 
 module.exports = router;
